dc9106ac887a1c022678c4e4d02ce6e5970855e9
theapril-studio <thanhvuttv@gmail.com>
2024-03-31 10:34:42 +0700

    Update logo

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,19 +1,20 @@
-import React from "react";
+import EzTradingLogo from "@/assets/eztrading-logo.png";
 import Image from "next/image";
+import React from "react";
 
 export const Logo = () => {
   return (
     <span className="self-center flex justify-center items-center ml-2 rtl:ml-0 rtl:mr-2 text-2xl md:text-3xl gap-3 font-bold whitespace-nowrap ">
-      <div className="relative w-10 h-10">
-        {/* <Image
-          src={"/images/logo.png"}
-          alt="EZHOUZE logo"
+      <div className="relative h-12 w-36">
+        <Image
+          src={EzTradingLogo}
+          alt="EZTrading logo"
           fill
           className="rounded-md"
           sizes="(max-width: 0px) 100px, (max-width: 0px) 100px, 100px"
-        /> */}
+        />
       </div>
-      <p className="hidden lg:block text-primary text-2xl">EZTRADING</p>
+      {/* <p className="hidden lg:block text-primary text-2xl">EZTRADING</p> */}
     </span>
   );
 };
